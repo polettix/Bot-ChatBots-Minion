91d72943247ca234dc65727c0949db07935d946f
Flavio Poletti <flavio@polettix.it>
2016-09-28 19:08:05 +0200

    Set alternative implementation

--- lib/Bot/ChatBots/Minion.pm
+++ lib/Bot/ChatBots/Minion.pm
@@ -5,13 +5,63 @@ use Ouch;
 
 use Mojo::Base 'Mojolicious::Plugin';
 use Log::Any ();
+use Bot::ChatBots::Utils qw< pipeline resolve_module >;
+
+has minion   => sub { ouch 500, 'no minion set' };
+has name     => sub { shift->typename };
+has prefix   => 'Bot::ChatBots';
+has typename => sub { return ref($_[0]) || $_[0] };
+
+sub logger { return Log::Any->get_logger };
+
+sub register {
+   my ($self, $app, $conf) = @_;
+   $self->minion($app->minion);
+   $self->name($conf->{name}) if exists $conf->{name};
+   $self->prefix($conf->{prefix}) if exists $conf->{prefix};
+   $self->typename($conf->{typename}) if exists $conf->{typename};
+   $app->helper('chatbots.minion' => sub { $self });
+   return $self;
+} ## end sub register
+
+sub wrapper {
+   my $self = shift;
+   my $args = (@_ && ref($_[0])) ? $_[0] : {@_};
+   my $topic = $args->{name} // $self->name
+     or ouch 500, 'no task name available for minion wrapper';
+
+   my $prefix = $args->{prefix} // $self->prefix;
+   my $ds = $args->{downstream} // $args->{processor}
+     or ouch 500, 'no processor provided for minion wrapper';
+   my $rds = ref $ds;
+   $ds = pipeline($rds eq 'ARRAY' ? @$ds : $ds, {prefix => $prefix})
+     if $rds ne 'CODE';
+
+   state $cache = {};
+   if (! exists $cache->{$topic}) {
+      $cache->{$topic} = sub {
+         my $record = shift;
+         $self->logger->info("enqueueing for $topic");
+         $self->minion->enqueue($topic, [$record]);
+         return $record;
+      };
+      $self->minion->add_task(
+         $topic,
+         sub {
+            $self->logger->info("dequeuing for $topic");
+            return $ds->($_[0]);
+         }
+      );
+   }
+   return $cache->{$topic};
+}
+
+42;
+__END__
 
 has dequeuer  => sub { ouch 500, 'no dequeuer set' };
 has logger    => sub { Log::Any->get_logger };
-has minion    => sub { ouch 500, 'no minion set' };
-has name      => sub { shift->typename };
 has sink => 0;
-has typename => sub { return ref($_[0]) || $_[0] };
 
 sub enqueue {
    my ($self, $record) = @_;
